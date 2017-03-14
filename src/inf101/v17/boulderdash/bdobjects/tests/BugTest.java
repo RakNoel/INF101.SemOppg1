7e063b87c4d09fd49fa07976d4327dbffbcc42d6
Oskar Leonhard Fretheim Leirvåg <oskar.leirvag@student.uib.no>
2017-03-14 02:47:17 +0100

    troubble with tests

--- src/inf101/v17/boulderdash/bdobjects/tests/BugTest.java
+++ src/inf101/v17/boulderdash/bdobjects/tests/BugTest.java
@@ -1,50 +1,121 @@
 package inf101.v17.boulderdash.bdobjects.tests;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import inf101.v17.boulderdash.Direction;
 import inf101.v17.boulderdash.Position;
-import inf101.v17.boulderdash.bdobjects.AbstractBDFallingObject;
 import inf101.v17.boulderdash.bdobjects.BDBug;
-import inf101.v17.boulderdash.bdobjects.BDDiamond;
 import inf101.v17.boulderdash.bdobjects.IBDObject;
 import inf101.v17.boulderdash.maps.BDMap;
 import inf101.v17.datastructures.IGrid;
 import inf101.v17.datastructures.MyGrid;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class BugTest {
 
-	private BDMap map;
+    private BDMap map;
 
-	@Before
-	public void setup() {
-	}
+    @Before
+    public void setup() {
+    }
 
-	@Test
-	public void bugMoves() {
-		IGrid<Character> grid = new MyGrid<>(4, 4, ' ');
-		grid.set(2, 2, 'b');
-		map = new BDMap(grid);
+    /**
+     * Test to see if a un blocked monster does move
+     */
+    @Test
+    public void bugMoves() {
+        IGrid<Character> grid = new MyGrid<>(4, 4, ' ');
+        grid.set(2, 2, 'b');
+        map = new BDMap(grid);
 
-		// find the bug
-		Position bugPos = new Position(2,2);
-		IBDObject bug = map.get(bugPos);
-		assertTrue(bug instanceof BDBug);
-		
-		for(int i = 0; i < 100; i++) {
-			map.step();
-			if(map.get(bugPos) != bug) { // bug has moved
-				// reported position should be different
-				assertNotEquals(bugPos, map.getPosition(bug));
-				// bug moved –  we're done
-				return;
-			}
-			
-		}
-		
-		fail("Bug should have moved in 100 steps!");
-	}
+        // find the bug
+        Position bugPos = new Position(2, 2);
+        IBDObject bug = map.get(bugPos);
+        assertTrue(bug instanceof BDBug);
+
+        for (int i = 0; i < 100; i++) {
+            map.step();
+            if (map.get(bugPos) != bug) { // bug has moved
+                // reported position should be different
+                assertNotEquals(bugPos, map.getPosition(bug));
+                // bug moved –  we're done
+                return;
+            }
+
+        }
+
+        fail("Bug should have moved in 100 steps!");
+    }
+
+    /**
+     * Test to see if a blocked monster is unable to move
+     */
+    @Test
+    public void bugLocked() {
+        IGrid<Character> grid = new MyGrid<>(3, 3, '*');
+        grid.set(1, 1, 'b');
+        map = new BDMap(grid);
+
+        // find the bug
+        Position bugPos = new Position(1, 1);
+        IBDObject bug = map.get(bugPos);
+        assertTrue(bug instanceof BDBug);
+
+        for (int i = 0; i < 100; i++) {
+            map.step();
+            if (map.get(bugPos) != bug) { // bug has moved
+                // reported position should be different
+                fail("Bug should NOT have moved!");
+            }
+        }
+
+        return;
+    }
+
+    /**
+     * Test to see if a bug moves as expected (WEST, NORTH, EAST, SOUTH)
+     */
+    @Test
+    public void bugExpectedMove() {
+        IGrid<Character> grid = new MyGrid<>(5, 5, ' ');
+        grid.set(2, 2, 'b');
+        map = new BDMap(grid);
+
+        // find the bug
+        Position bugPos = new Position(2, 2);
+        IBDObject bug = map.get(bugPos);
+        assertTrue(bug instanceof BDBug);
+
+        Position expected = new Position(2, 2);
+        Main:
+        for (int p = 1; p <= 4; p++)
+            for (int i = 0; i < 100; i++) {
+                map.step();
+                if (!map.getPosition(bug).equals(expected)) {
+                    //Bug moved once, check if right move
+                    System.out.println("Moved");
+                    switch (p) {
+                        case 1:
+                            expected = new Position(1, 2);
+                            break;
+                        case 2:
+                            expected = new Position(1, 1);
+                            break;
+                        case 3:
+                            expected = new Position(2, 1);
+                            break;
+                        case 4:
+                            expected = new Position(2, 2);
+                            break;
+                    }
+                    if (!map.getPosition(bug).equals(expected))
+                        fail("Monster not in correct spot, found in: " + map.getPosition(bug));
+                    else
+                        System.out.println("passed");
+                        continue Main;
+                }
+            }
+
+
+    }
 }
--- src/inf101/v17/boulderdash/maps/BDMap.java
+++ src/inf101/v17/boulderdash/maps/BDMap.java
@@ -1,291 +1,294 @@
 package inf101.v17.boulderdash.maps;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import inf101.v17.boulderdash.Direction;
 import inf101.v17.boulderdash.IllegalMoveException;
 import inf101.v17.boulderdash.Position;
-import inf101.v17.boulderdash.bdobjects.BDBug;
-import inf101.v17.boulderdash.bdobjects.BDDiamond;
-import inf101.v17.boulderdash.bdobjects.BDEmpty;
-import inf101.v17.boulderdash.bdobjects.BDPlayer;
-import inf101.v17.boulderdash.bdobjects.BDSand;
-import inf101.v17.boulderdash.bdobjects.BDWall;
-import inf101.v17.boulderdash.bdobjects.IBDObject;
+import inf101.v17.boulderdash.bdobjects.*;
 import inf101.v17.datastructures.IGrid;
 import inf101.v17.datastructures.MyGrid;
 
+import java.util.*;
+
 /**
  * An implementation of a map
- * 
- * @author larsjaffke
  *
+ * @author larsjaffke
  */
 public class BDMap {
 
-	/**
-	 * Stores the data of the map
-	 */
-	protected IGrid<IBDObject> grid;
-	/**
-	 * A separate reference to the player, since it is accessed quite
-	 * frequently.
-	 */
-	protected BDPlayer player;
+    /**
+     * Stores the data of the map
+     */
+    protected IGrid<IBDObject> grid;
 
-	/**
-	 * Main constructor of this class.
-	 * 
-	 * @param map
-	 *            A grid of characters, where each character represents a type
-	 *            of {@link #IBDObject}: ' ' - {@link #BDEmpty}, '*' -
-	 *            {@link BDWall}, '#' - {@link #BDSand}, 'd' -
-	 *            {@link #BDDiamond}, 'b' - {@link BDBug}, 'r' -
-	 *            {@link #BDRock}, 'p' - {@link #BDPlayer}
-	 * 
-	 * @param player
-	 *            The player object has to be initialized separately.
-	 */
-	public BDMap(IGrid<Character> map) {
-		grid = new MyGrid<IBDObject>(map.getWidth(), map.getHeight(), null);
-		this.player = new BDPlayer(this);
-		fillGrid(map);
-	}
+    /**
+     * Stores position of each object with their hash
+     */
+    protected HashMap<IBDObject, Position> hashMap;
 
-	/**
-	 * True of the object can move in direction dir, false otherwise.
-	 * 
-	 * @param obj
-	 * @param dir
-	 * @return
-	 */
-	public boolean canGo(IBDObject obj, Direction dir) {
-		return canGo(this.getPosition(obj), dir);
-	}
+    /**
+     * A separate reference to the player, since it is accessed quite
+     * frequently.
+     */
+    protected BDPlayer player;
 
-	/**
-	 * True if an object can move to positio (x, y), false otherwise.
-	 * 
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	public boolean canGo(int x, int y) {
-		if (!isValidPosition(x, y)) {
-			return false;
-		}
-		if (grid.get(x, y) instanceof BDWall) {
-			return false;
-		}
-		return true;
-	}
+    /**
+     * Main constructor of this class.
+     *
+     * @param map    A grid of characters, where each character represents a type
+     *               of {@link #IBDObject}: ' ' - {@link #BDEmpty}, '*' -
+     *               {@link BDWall}, '#' - {@link #BDSand}, 'd' -
+     *               {@link #BDDiamond}, 'b' - {@link BDBug}, 'r' -
+     *               {@link #BDRock}, 'p' - {@link #BDPlayer}
+     * @param player The player object has to be initialized separately.
+     */
+    public BDMap(IGrid<Character> map) {
+        grid = new MyGrid<IBDObject>(map.getWidth(), map.getHeight(), null);
+        hashMap = new HashMap<IBDObject, Position>();
+        this.player = new BDPlayer(this);
+        fillGrid(map);
+    }
 
-	/**
-	 * See {@link #canGo(int, int)}
-	 * 
-	 * @param pos
-	 * @return
-	 */
-	public boolean canGo(Position pos) {
-		return canGo(pos.getX(), pos.getY());
-	}
+    /**
+     * True of the object can move in direction dir, false otherwise.
+     *
+     * @param obj
+     * @param dir
+     * @return
+     */
+    public boolean canGo(IBDObject obj, Direction dir) {
+        return canGo(this.getPosition(obj), dir);
+    }
 
-	/**
-	 * Cf. {@link #canGo(IBDObject, Direction)}
-	 * 
-	 * @param pos
-	 * @param dir
-	 * @return
-	 */
-	public boolean canGo(Position pos, Direction dir) {
-		switch (dir) {
-		case NORTH:
-			return canGo(pos.getX(), pos.getY() + 1);
-		case SOUTH:
-			return canGo(pos.getX(), pos.getY() - 1);
-		case EAST:
-			return canGo(pos.getX() + 1, pos.getY());
-		case WEST:
-			return canGo(pos.getX() - 1, pos.getY());
-		}
-		throw new IllegalArgumentException();
-	}
+    /**
+     * True if an object can move to positio (x, y), false otherwise.
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public boolean canGo(int x, int y) {
+        if (!isValidPosition(x, y)) {
+            return false;
+        }
+        if (grid.get(x, y) instanceof BDWall) {
+            return false;
+        }
+        return true;
+    }
 
-	/**
-	 * This method is used to initialize the map with a given map of charaters,
-	 * see also {@link #BDMap(IGrid, BDPlayer)}.
-	 * 
-	 * @param inputmap
-	 */
-	private void fillGrid(IGrid<Character> inputmap) {
-		int width = inputmap.getWidth();
-		int height = inputmap.getHeight();
+    /**
+     * See {@link #canGo(int, int)}
+     *
+     * @param pos
+     * @return
+     */
+    public boolean canGo(Position pos) {
+        return canGo(pos.getX(), pos.getY());
+    }
 
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				IBDObject obj = makeObject(inputmap.get(x, y), x, y);
-				grid.set(x, y, obj);
-			}
-		}
-	}
+    /**
+     * Cf. {@link #canGo(IBDObject, Direction)}
+     *
+     * @param pos
+     * @param dir
+     * @return
+     */
+    public boolean canGo(Position pos, Direction dir) {
+        switch (dir) {
+            case NORTH:
+                return canGo(pos.getX(), pos.getY() + 1);
+            case SOUTH:
+                return canGo(pos.getX(), pos.getY() - 1);
+            case EAST:
+                return canGo(pos.getX() + 1, pos.getY());
+            case WEST:
+                return canGo(pos.getX() - 1, pos.getY());
+        }
+        throw new IllegalArgumentException();
+    }
 
-	/**
-	 * Initialize an object according to the given character, see also
-	 * {@link #BDMap(IGrid, BDPlayer)}.
-	 * 
-	 * @param c
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	private IBDObject makeObject(Character c, int x, int y) {
-		if (c == 'p') {
-			return this.player;
-		} else if (c == '*') {
-			return new BDWall(this);
-		} else if (c == 'b') {
-			try {
-				return new BDBug(this, new Position(x, y), 1, 10);
-			} catch (IllegalMoveException e) {
-				e.printStackTrace();
-			}
-		} else if (c == '#') {
-			return new BDSand(this);
-		} else if (c == ' ') {
-			return new BDEmpty(this);
-		} else if (c == 'd') {
-			return new BDDiamond(this);
-		}
-		System.err.println("Illegal character in map definition at (" + x + ", " + y + "): '" + c + "'");
-		return new BDEmpty(this);
-		// alternatively, throw an exception
-		// throw new IllegalArgumentException();
-	}
+    /**
+     * This method is used to initialize the map with a given map of charaters,
+     * see also {@link #BDMap(IGrid, BDPlayer)}.
+     *
+     * @param inputmap
+     */
+    private void fillGrid(IGrid<Character> inputmap) {
+        int width = inputmap.getWidth();
+        int height = inputmap.getHeight();
 
-	/**
-	 * get the object in (x, y)
-	 * 
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	public IBDObject get(int x, int y) {
-		return grid.get(x, y);
-	}
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                IBDObject obj = makeObject(inputmap.get(x, y), x, y);
+                grid.set(x, y, obj);
+                hashMap.put(obj, new Position(x,y));
+            }
+        }
+    }
 
-	/**
-	 * see {@link #get(int, int)}
-	 * 
-	 * @param pos
-	 * @return
-	 */
-	public IBDObject get(Position pos) {
-		return grid.get(pos.getX(), pos.getY());
-	}
+    /**
+     * Initialize an object according to the given character, see also
+     * {@link #BDMap(IGrid, BDPlayer)}.
+     *
+     * @param c
+     * @param x
+     * @param y
+     * @return
+     */
+    private IBDObject makeObject(Character c, int x, int y) {
+        if (c == 'p') {
+            return this.player;
+        } else if (c == '*') {
+            return new BDWall(this);
+        } else if (c == 'b') {
+            try {
+                return new BDBug(this, new Position(x, y), 1, 10);
+            } catch (IllegalMoveException e) {
+                e.printStackTrace();
+            }
+        } else if (c == '#') {
+            return new BDSand(this);
+        } else if (c == ' ') {
+            return new BDEmpty(this);
+        } else if (c == 'd') {
+            return new BDDiamond(this);
+        }
+        System.err.println("Illegal character in map definition at (" + x + ", " + y + "): '" + c + "'");
+        return new BDEmpty(this);
+        // alternatively, throw an exception
+        // throw new IllegalArgumentException();
+    }
 
-	/**
-	 * Returns the height of this map.
-	 * 
-	 * @return
-	 */
-	public int getHeight() {
-		return grid.getHeight();
-	}
+    /**
+     * get the object in (x, y)
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public IBDObject get(int x, int y) {
+        return grid.get(x, y);
+    }
 
-	/**
-	 * Given a position pos, this method computes the nearest n empty fields.
-	 * This method is used when a bug dies and had to spread a certain number of
-	 * diamonds in neighboring fields.
-	 * 
-	 * @param pos
-	 * @param n
-	 * @return
-	 */
-	public List<Position> getNearestEmpty(Position pos, int n) {
-		List<Position> empty = new ArrayList<>();
-		for (int x = 0; x < getWidth(); x++) {
-			for (int y = 0; y < getHeight(); y++) {
-				if (get(x, y) instanceof BDEmpty) {
-					empty.add(new Position(x, y));
-				}
-			}
-		}
-		if (empty.size() < n) {
-			throw new IllegalArgumentException("There are less than " + n + " empty fields on this map");
-		}
-		Collections.sort(empty, new Comparator<Position>() {
+    /**
+     * see {@link #get(int, int)}
+     *
+     * @param pos
+     * @return
+     */
+    public IBDObject get(Position pos) {
+        return grid.get(pos.getX(), pos.getY());
+    }
 
-			@Override
-			public int compare(Position o1, Position o2) {
-				return pos.distanceTo(o1) - pos.distanceTo(o2);
-			}
-		});
-		List<Position> nearby = new ArrayList<>(n);
-		for (int i = 0; i < n; i++) {
-			nearby.add(empty.get(i));
-		}
-		return nearby;
-	}
+    /**
+     * Returns the height of this map.
+     *
+     * @return
+     */
+    public int getHeight() {
+        return grid.getHeight();
+    }
 
-	/**
-	 * Get the player in this map.
-	 * 
-	 * @return
-	 */
-	public BDPlayer getPlayer() {
-		return player;
-	}
+    /**
+     * Given a position pos, this method computes the nearest n empty fields.
+     * This method is used when a bug dies and had to spread a certain number of
+     * diamonds in neighboring fields.
+     *
+     * @param pos
+     * @param n
+     * @return
+     */
+    public List<Position> getNearestEmpty(Position pos, int n) {
+        List<Position> empty = new ArrayList<>();
+        for (int x = 0; x < getWidth(); x++) {
+            for (int y = 0; y < getHeight(); y++) {
+                if (get(x, y) instanceof BDEmpty) {
+                    empty.add(new Position(x, y));
+                }
+            }
+        }
+        if (empty.size() < n) {
+            throw new IllegalArgumentException("There are less than " + n + " empty fields on this map");
+        }
+        Collections.sort(empty, new Comparator<Position>() {
 
-	/**
-	 * Get the position of an object in this map
-	 * 
-	 * @param object
-	 * @return
-	 */
-	public Position getPosition(IBDObject object) {
-		// TODO
-		
-		return null;
-	}
+            @Override
+            public int compare(Position o1, Position o2) {
+                return pos.distanceTo(o1) - pos.distanceTo(o2);
+            }
+        });
+        List<Position> nearby = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            nearby.add(empty.get(i));
+        }
+        return nearby;
+    }
 
-	/**
-	 * get the width of this map.
-	 * 
-	 * @return
-	 */
-	public int getWidth() {
-		return grid.getWidth();
-	}
+    /**
+     * Get the player in this map.
+     *
+     * @return
+     */
+    public BDPlayer getPlayer() {
+        return player;
+    }
 
-	/**
-	 * Check whether (x, y) is a valid position in this map.
-	 * 
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	private boolean isValidPosition(int x, int y) {
-		return x > -1 && x < grid.getWidth() && y > -1 && y < grid.getHeight();
-	}
+    /**
+     * Get the position of an object in this map
+     *
+     * @param object
+     * @return
+     */
+    public Position getPosition(IBDObject object) {
+        // TODO
+//		for (int x = 0; x < this.getHeight(); x++)
+//			for (int y = 0; y < this.getWidth(); y++)
+//				if(this.get(x,y) == object)
+//					return new Position(x, y);
 
-	/**
-	 * set tile (x, y) to element.
-	 * 
-	 * @param x
-	 * @param y
-	 * @param element
-	 */
-	public void set(int x, int y, IBDObject element) {
-		if (!isValidPosition(x, y)) {
-			throw new IndexOutOfBoundsException();
-		}
-		grid.set(x, y, element);
-	}
+        return hashMap.get(object);
+    }
 
-	public void step() {
-		// TODO
-		
-	}
+    /**
+     * get the width of this map.
+     *
+     * @return
+     */
+    public int getWidth() {
+        return grid.getWidth();
+    }
+
+    /**
+     * Check whether (x, y) is a valid position in this map.
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    private boolean isValidPosition(int x, int y) {
+        return x > -1 && x < grid.getWidth() && y > -1 && y < grid.getHeight();
+    }
+
+    /**
+     * set tile (x, y) to element.
+     *
+     * @param x
+     * @param y
+     * @param element
+     */
+    public void set(int x, int y, IBDObject element) {
+        if (!isValidPosition(x, y)) {
+            throw new IndexOutOfBoundsException();
+        }
+        grid.set(x, y, element);
+        hashMap.put(element, new Position(x, y));
+    }
+
+    public void step() {
+        // TODO
+        for (int x = 0; x < this.getWidth(); x++)
+            for (int y = 0; y < this.getWidth(); y++)
+                this.get(x,y).step();
+    }
 }

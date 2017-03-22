056ded96b79fd99c8c7fe50ce4ebc5e359ed56b8
Oskar Leonhard Fretheim Leirvåg <oskar.leirvag@student.uib.no>
2017-03-22 09:11:17 +0100

    adding playertests

--- src/inf101/v17/boulderdash/bdobjects/tests/PlayerTest.java
+++ src/inf101/v17/boulderdash/bdobjects/tests/PlayerTest.java
@@ -1,5 +1,25 @@
 package inf101.v17.boulderdash.bdobjects.tests;
 
+import inf101.v17.boulderdash.bdobjects.*;
+import inf101.v17.boulderdash.maps.BDMap;
+import inf101.v17.datastructures.IGrid;
+import inf101.v17.datastructures.MyGrid;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 //Created by RakNoel, 16.03.2017.
 public class PlayerTest {
+    private BDMap map;
+
+    @Test
+    public void basicPlayerMovement(){
+        IGrid<Character> grid = new MyGrid<>(9, 9, ' ');
+        grid.set(1, 0, 'p');
+        map = new BDMap(grid);
+
+        assertTrue(map.get(1,0) instanceof BDPlayer);
+
+
+    }
 }
